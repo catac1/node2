ca62303205f2e8729c556e95684190af223ca3fd
kst <kst@users.noreply.github.com>
2026-04-28 15:57:48 +0900

    delete board

--- routes/board.js
+++ routes/board.js
@@ -3,7 +3,38 @@ import pool from '../db.js';
 
 const router = express.Router();
 
-// url => 127.0.0.1:8081/api/board/selectlist.json
+// url => DELETE 127.0.0.1:8080/api/board/deleteone.json
+// body => {"no": 5}
+router.delete('/deleteone.json', async (req, res) => {
+    try {
+        const { no } = req.body;
+        const sql = 'delete from board where no=?';
+        const [result] = await pool.query(sql, [no]);
+        console.log(result);
+        return res.send({ result: result });
+    }
+    catch (err) {
+        return res.status(500).send({ err: err });
+    }
+});
+
+// url => PUT 127.0.0.1:8080/api/board/updateone.json?no=5
+// body => {"title": "제목변경", "content": "내용변경", "writer": "변경자"}
+router.put('/updateone.json', async (req, res) => {
+    try {
+        const { no } = req.query;
+        const { title, content, writer } = req.body;
+        const sql = 'update board set title=?, content=?, writer=? where no=?';
+        const [result] = await pool.query(sql, [title, content, writer, no]);
+        console.log(result);
+        return res.send({ result: result });
+    }
+    catch (err) {
+        return res.status(500).send({ err: err });
+    }
+});
+
+// url => GET 127.0.0.1:8081/api/board/selectlist.json
 router.get('/selectlist.json', async (req, res) => {
     try {
         const { title, content, writer } = req.body;
@@ -32,7 +63,5 @@ router.post('/insert.json', async (req, res) => {
         return res.send({ err: err });
     }
 });
-
-
 
 export default router;
