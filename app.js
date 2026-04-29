b36a2ffc736764ff357a81e3246e8efa96f518d2
kst <kst@users.noreply.github.com>
2026-04-29 17:09:07 +0900

    프론트 통합

--- app.js
+++ app.js
@@ -6,12 +6,20 @@ import itemimageRouter from './routes/itemimage.js';
 // import password_fixup from './routes/password_fixup.js';
 import password_reset from './routes/password_reset.js';
 
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const buildPath = path.join(__dirname, './build');
+
 const app = express();
 const port = 8080;
 const host = '0.0.0.0';
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(buildPath));
 
 app.use('/api/board', boardRouter);
 app.use('/api/customer', customerRouter);
@@ -21,6 +29,13 @@ app.use('/api/itemimage', itemimageRouter);
 // app.use('/api/customer', pimageasswordimage_fixup);
 app.use('/api/customer', password_reset);
 app.use('/api/admin', password_reset);
+
+app.use((req, res, next) => {
+    if (req.path.startsWith('/api')) {
+        return next();
+    }
+    res.sendFile(path.join(buildPath, 'index.html'));
+})
 
 
 app.get('/', (req, res) => {
